0a85fab8963871ca78706195142260f276a3ef6f
Susan Delmoor-Rodriguez <susan@bfuturedigital.net>
2015-08-11 12:29:52 +0000

    lesson notes

--- js/script.js
+++ js/script.js
@@ -1,22 +1,99 @@
 // Variables (var)
+//1 Declaring separately
+// var x;
+// var y;
+// var square_area;  //snake_case
+// var myVariable;  // camelCase
 
-// Constants (const)
+// variables can not start with numbers and can not have spaces
+// Assign a value to the variables with = symbol
 
-// Type of Data (typeof)
+// x = 21;
+// y = 2;
+
+//Can also declare and assign a value at the same time
+//var x = 21;
+
+// 2 Declare multiple variables at once with comma separation
+//var x,y,square_area;
+
+// At this stage we can perform normal math operations
+
+// myMath = x + y;
+
+// moreMath = x*y;
+
+// even_Moremath = x%y;
+
+//console.log("myMath equals " + myMath);
+//console.log("moreMath equals " + moreMath);
+//console.log("even_Moremath equals " + even_Moremath);
+
+// What is happening in the console.log method ?
+// + what is the plus doing
+
+//Data Types that can be stored in Variables
+//Numbers
+//Strings (text)  'hello' or "hello"
+//Boolean true or false
+// null (empty)
+//undefined ( not yet defined)
+
+// var numberofStudents = 25;
+// console.log("1st variable assignment " + numberofStudents);
+
+// numberofStudents = "twentyfive";
+// console.log("2nd variable assignment " + numberofStudents);
+
+// //numberofStudents = null;
+// //console.log("3rd variable assignment " + numberofStudents);
+
+//How to check for data type
+// typeof numberofStudents;
+
+// whatDataType = typeof numberofStudents;
+// console.log ("numberofStudents is a " + whatDataType);
 
 
-// String Concatination
+//To convert a string to a whole number/integer
+// combineStuff = parseInt('10') + 5;
+// console.log(combineStuff);
+
+// To convert to a floating number ( decimal)
+// var gallonsGas = parseFloat('10.25');
+// console.log(" gallonsGas = " + gallonsGas);
+
+//Let's check to see what the data type is
+// whatDataType = typeof gallonsGas;
+// console.log ("gallonsGas is a " + whatDataType);
+
+//Dialog Boxes
+
+//alert ("Warning, It looks like your computer may have a virus");// outputs a string
+
+//confirm  (Boolean value)
+//var confirmDel = confirm('Are you sure you want to delete this?'); //true or false
+
+//prompt
+var numberinGrade = 0;
+var numberinGrade = prompt("Enter number of in the 11th grade: "); //captures the value inserted.
+console.log ("Number of students in 11th grade is  " + numberinGrade);
+// DonutsforStudents = numberinGrade * 2;
+// console.log ("Number of students that get donuts  " + DonutsforStudents);
+
+//What's wrong
+// convertedStudents = Number(numberinGrade);
+// DonutsforStudents = convertedStudents * 2;
+// console.log ("Number of students that get donuts  " + DonutsforStudents);
 
 
-// Data Type Conversion (parseInt, parseFloat)
 
 
-// String Methods (example: .toUpperCase())
 
-
-// Dialog Boxes (alert, confirm, prompt)
-
-
+// Diaconsole.log Boxes (alert, confirm, prompt)
+// square_area = x*y
+// console.log(square_area);
+// alert(square_area);
 //Conditional Statements (if/else)
 
   // if() {
