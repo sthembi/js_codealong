1c95d0887e698b4239d7704abc8285cd3c1e872b
Susan Delmoor-Rodriguez <susan@bfuturedigital.net>
2015-08-16 23:21:37 +0000

    committing changes from class lesson

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 // Variables (var)
 //1 Declaring separately
-// var x;
+var x;
+var age;
+//var studentsAge;
 // var y;
 // var square_area;  //snake_case
 // var myVariable;  // camelCase
 
 // variables can not start with numbers and can not have spaces
 // Assign a value to the variables with = symbol
+age = 19;
+var studentsAge = 19;
+var numberofStudents  = 10;
 
-// x = 21;
-// y = 2;
 
 //Can also declare and assign a value at the same time
 //var x = 21;
@@ -17,9 +20,18 @@
 // 2 Declare multiple variables at once with comma separation
 //var x,y,square_area;
 
+var total = studentsAge * numberofStudents;
+//console.log(total);
+
+var x = 31;
+
+var y = 3;
+
+//console.log ( x % y);
+
 // At this stage we can perform normal math operations
 
-// myMath = x + y;
+ var myMath = x + y;
 
 // moreMath = x*y;
 
@@ -39,25 +51,35 @@
 // null (empty)
 //undefined ( not yet defined)
 
-// var numberofStudents = 25;
-// console.log("1st variable assignment " + numberofStudents);
+//var typeofCar = "infiniti";
+//var happyMood = true;
+//var happyMood = null;
 
-// numberofStudents = "twentyfive";
+//console.log (happyMood);
+
+var numberofStudents = 25;
+//console.log("1st variable assignment " + numberofStudents);
+
+// numberofStudents = "25";
 // console.log("2nd variable assignment " + numberofStudents);
 
 // //numberofStudents = null;
 // //console.log("3rd variable assignment " + numberofStudents);
 
 //How to check for data type
-// typeof numberofStudents;
+//typeof numberofStudents;
 
-// whatDataType = typeof numberofStudents;
-// console.log ("numberofStudents is a " + whatDataType);
+//whatDataType = typeof numberofStudents;
+//console.log ("numberofStudents is a " + whatDataType);
 
+//var x = "10";
 
 //To convert a string to a whole number/integer
-// combineStuff = parseInt('10') + 5;
-// console.log(combineStuff);
+
+//parseInt(x);
+
+//combineStuff = parseInt('10') + 5;
+//console.log(combineStuff);
 
 // To convert to a floating number ( decimal)
 // var gallonsGas = parseFloat('10.25');
@@ -68,21 +90,34 @@
 // console.log ("gallonsGas is a " + whatDataType);
 
 //Dialog Boxes
-
-//alert ("Warning, It looks like your computer may have a virus");// outputs a string
+//alert("some text goes here");
+//alert("Warning, It looks like your computer may have a virus");// outputs a string
 
 //confirm  (Boolean value)
-//var confirmDel = confirm('Are you sure you want to delete this?'); //true or false
+//confirm("are you sure?")
+//confirm('Are you sure you want to delete this?'); //true or false
 
-//prompt
-var numberinGrade = 0;
-var numberinGrade = prompt("Enter number of in the 11th grade: "); //captures the value inserted.
+// var height, width, area;
+// height = 5;
+// width = 5;
+// area = height * width;
+//console.log(area);
+
+//prompts
+//var numberinGrade = 0;
+var numberinGrade = prompt("Enter number of students in the 11th grade: "); //captures the value inserted.
 console.log ("Number of students in 11th grade is  " + numberinGrade);
-// DonutsforStudents = numberinGrade * 2;
-// console.log ("Number of students that get donuts  " + DonutsforStudents);
+
+DonutsforStudents = numberinGrade * 2;
+console.log ("Number of donuts the 11th grade students get   " + DonutsforStudents);
+
+
+
+
 
 //What's wrong
-// convertedStudents = Number(numberinGrade);
+
+// convertedStudents = parseInt(numberinGrade);
 // DonutsforStudents = convertedStudents * 2;
 // console.log ("Number of students that get donuts  " + DonutsforStudents);
 
